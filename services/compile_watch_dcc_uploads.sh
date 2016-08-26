be0d73c321f0eb366aeb3734f225d61c3e1fb889
Leandro Hermida <leandro.hermida@nih.gov>
2016-08-26 10:58:36 -0400

    Updated watch service compiler script to be able to compile both services

--- services/compile_watch_dcc_uploads.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmd='pp -M File::ChangeNotify -M File::ChangeNotify::Watcher -M Email::Sender -M Email::Sender::Role::CommonSending -M Throwable -M Throwable::Error -M StackTrace::Auto -M List::MoreUtils::PP -M Email::Abstract::EmailSimple -c -o watch_dcc_uploads watch_dcc_uploads.pl'
-echo $cmd
-$cmd
