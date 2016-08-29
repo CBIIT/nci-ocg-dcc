1f28a48c53cb61151345757dd9b3395fee892e97
Leandro Hermida <leandro.hermida@nih.gov>
2016-08-29 10:07:46 -0400

    Start project common config

---
+++ common/conf/common_conf.pl
@@ -0,0 +1,42 @@
+# common config
+{
+    'program_names' => [qw(
+        TARGET
+        CGCI
+        CTD2
+    )],
+    'program_project_names' => {
+        'TARGET' => [qw(
+            ALL
+            AML
+            CCSK
+            MDLS-NBL
+            MDLS-PPTP
+            NBL
+            OS
+            OS-Brazil
+            OS-Toronto
+            RT
+            WT
+            Resources
+        )],
+        'CGCI' => [qw(
+            BLGSP
+            HTMCP-CC
+            HTMCP-DLBCL
+            HTMCP-LC
+            MB
+            NHL
+            Resources
+        )],
+    },
+    'seq_data_types' => [qw(
+        Bisulfite-seq
+        ChIP-seq
+        miRNA-seq
+        mRNA-seq
+        Targeted-Capture
+        WGS
+        WXS
+    )],
+}
