147a15681580da7a7f31343c48ab74975d4e2e8e
Leandro Hermida <leandro.hermida@nih.gov>
2017-04-07 16:15:21 -0400

    Various output improvements and added custom output dir

--- sra/get_sra_xml.pl
+++ sra/get_sra_xml.pl
@@ -3,21 +3,18 @@
 use strict;
 use warnings;
 use Cwd qw(cwd);
-use Getopt::Long qw(:config auto_help auto_version);
+use File::Path 2.11 qw( make_path );
+use Getopt::Long qw( :config auto_help auto_version );
 use LWP::UserAgent;
-use Pod::Usage qw(pod2usage);
+use Pod::Usage qw( pod2usage );
+use Term::ANSIColor;
 use XML::Tidy;
-use Data::Dumper;
 
 our $VERSION = '0.1';
 
 # Unbuffer error and output streams (make sure STDOUT is last so that it remains the default filehandle)
 select(STDERR); $| = 1;
 select(STDOUT); $| = 1;
-
-$Data::Dumper::Sortkeys = 1;
-$Data::Dumper::Terse = 1;
-$Data::Dumper::Deepcopy = 1;
 
 # config
 my $xml_tidy_indent_str = ' ' x 4;
@@ -33,22 +30,37 @@ my $run_id_file = shift(@ARGV) or pod2usage(
     -message => 'Run ID file is required parameter',
     -verbose => 0,
 );
+my $output_dir = @ARGV ? shift(@ARGV) : cwd();
+if (-d $output_dir) {
+    make_path($output_dir, { chmod => 0770 })
+        or die +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+               ": could not create $output_dir: $!";
+}
 my $ua = LWP::UserAgent->new();
-open(my $in_fh, '<', $run_id_file) or die "ERROR: couldn't open $run_id_file: $!";
-while (my $run_id = <$in_fh>) {
+my $num_fetched = 0;
+open(my $ids_fh, '<', $run_id_file)
+    or die +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+           ": couldn't open $run_id_file: $!";
+while (my $run_id = <$ids_fh>) {
     $run_id =~ s/\s+//g;
     my $response = $ua->get(
         "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=xml&term=$run_id"
     );
     if (!$response->is_success) {
-        print "ERROR: failed to get SRA $run_id experiment package XML: ", $response->status_line, "\n";
+        warn +(-t STDERR ? colored('ERROR', 'red') : 'ERROR'),
+              ": failed to get SRA $run_id experiment package XML: ",
+              $response->status_line, "\n";
         next;
     }
     my $xml_tidy_obj = XML::Tidy->new('xml' => $response->decoded_content);
     $xml_tidy_obj->tidy($xml_tidy_indent_str);
-    $xml_tidy_obj->write(cwd() . "/${run_id}.xml");
+    $xml_tidy_obj->write("$output_dir/${run_id}.xml");
+    $num_fetched++;
+    print "\r$num_fetched xmls fetched" if -t STDOUT;
 }
-close($in_fh);
+close($ids_fh);
+print "$num_fetched xmls fetched" unless -t STDOUT and $num_fetched;
+print "\n";
 
 __END__
 
@@ -58,10 +70,11 @@ get_sra_xml.pl - SRA-XML Downloader
 
 =head1 SYNOPSIS
 
- get_sra_xml.pl [options] <run id file>
+ get_sra_xml.pl [options] <run id file> <output dir>
  
  Parameters:
-    <run id file>       File with list of SRA run IDs
+    <run id file>       File with list of SRA run IDs (required)
+    <output dir>        Output directory path (optional, default $PWD)
  
  Options:
     --verbose           Be verbose
