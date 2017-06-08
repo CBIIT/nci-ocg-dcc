a11b5f2928ff9b02cbfaa45a6a702414439648d9
Leandro Hermida <leandro.hermida@nih.gov>
2017-06-08 09:56:28 -0400

    Fixed $CACHE_DIR resolution when missing $HOME env var

--- lib/perl5/NCI/OCGDCC/Config.pm
+++ lib/perl5/NCI/OCGDCC/Config.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use FindBin;
 use Const::Fast;
-use Cwd qw( abs_path );
+use Cwd qw( cwd abs_path );
 require Exporter;
 
 our @ISA = qw(Exporter);
@@ -21,7 +21,7 @@ our %EXPORT_TAGS = (
 our $VERSION = '0.1';
 
 const our $SCRIPT_BASE_DIR          => abs_path("$FindBin::Bin/..");
-const our $CACHE_DIR                => "$ENV{'HOME'}/.ocg-dcc";
+const our $CACHE_DIR                => defined($ENV{HOME}) ? "$ENV{HOME}/.ocg-dcc" : cwd() . '/ocg-dcc-cache';
 const our $OCG_CASE_REGEXP          => qr/[A-Z]+-\d{2}(?:-\d{2})?-[A-Z0-9]+/;
 const our $OCG_CGI_CASE_DIR_REGEXP  => qr/${OCG_CASE_REGEXP}(?:(?:-|_)\d+)?/;
 const our $OCG_BARCODE_REGEXP       => qr/${OCG_CASE_REGEXP}-\d{2}(?:\.\d+)?[A-Z](?:\.\d+)?-\d{2}[A-Z]/;
